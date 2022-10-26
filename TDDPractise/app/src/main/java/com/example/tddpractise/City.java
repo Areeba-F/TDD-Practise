c21db866e8a1c7013282195e2c5108cf116798c2
“Areeba <areeba.fazalxx11@gmail.com>
2022-10-25 18:12:16 -0600

    added project

---
+++ TDDPractise/app/src/main/java/com/example/tddpractise/City.java
@@ -0,0 +1,19 @@
+package com.example.tddpractise;
+
+public class City {
+    private String city;
+    private String province;
+
+    public City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
+}
